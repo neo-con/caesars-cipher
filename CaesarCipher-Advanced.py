528be88accfac3c378d2aa268e3beef56342b8f5
neo-con <41864395+neo-con@users.noreply.github.com>
2024-04-22 13:56:11 -0400

    Refactored

--- CaesarCipher-Advanced.py
+++ CaesarCipher-Advanced.py
@@ -1,40 +1,41 @@
-#Caesar's Cipher-Advanced.py
-def encrypt(message, key):
-    result = ""
-    for letter in message:
-        num = ord(letter) + key
-        result += chr(num)
-    return result
+class CaesarCipher:
+    def __init__(self):
+        self.key = self.key_req()
 
-def decrypt(message, key):
-    result = ""
-    for letter in message:
-        num = ord(letter) - key
-        result += chr(num)
-    return result
+    def encrypt(self, message):
+        result = ""
+        for letter in message:
+            num = ord(letter) + self.key
+            result += chr(num)
+        return result
 
+    def decrypt(self, message):
+        result = ""
+        for letter in message:
+            num = ord(letter) - self.key
+            result += chr(num)
+        return result
 
-#The valid range for the chr() function is 0 through 1,114,111. However, we will use roughly half of that range.
-def key_req():
-    key = input("Enter a key: ")
-    if key.isdigit():
-        key = int(key)
-        if key in range(1,555000):
-            return key
-        else:
-            print("Key must be in range of 1-555000")
-            key_req()
-    else:
-        print("Key must be a number")
-        key_req()    
-
+    def key_req(self):
+        while True:
+            key = input("Enter a key (1-555000): ")
+            if key.isdigit():
+                key = int(key)
+                if 1 <= key <= 555000:
+                    return key
+                else:
+                    print("Key must be in the range of 1-555000.")
+            else:
+                print("Key must be a number.")
 
 def main():
     message = input("Enter a message: ")
-    key = key_req()
-    print("Encrypted message:", encrypt(message, key))
-    print("Decrypted message:", decrypt(encrypt(message, key), key))
+    cipher = CaesarCipher()
+    encrypted_message = cipher.encrypt(message)
+    print("Encrypted message:", encrypted_message)
+    print("Decrypted message:", cipher.decrypt(encrypted_message))
 
-main()
+if __name__ == "__main__":
+    main()
 
 
--- CaesarCipher-Basic.py
+++ CaesarCipher-Basic.py
@@ -1,63 +1,38 @@
-#Caesar's Cipher
-def encrypt(message, key):
-    result = ""
-    for letter in message:
-        num = ord(letter) + key
-        if letter.isalpha():
-            if letter.isupper():
-                if num > ord('Z'):
-                    num -= 26
-                elif num < ord('A'):
-                    num += 26
-            elif letter.islower():
-                if num > ord('z'):
-                    num -= 26
-                elif num < ord('a'):
-                    num += 26
-            result += chr(num)
-        else:
-            result += letter
-    return result
+class CaesarCipher:
+    def __init__(self, key=None):
+        self.key = key if key is not None else self.key_req()
 
-def decrypt(message, key):
-    result = ""
-    for letter in message:
-        num = ord(letter) - key
-        if letter.isalpha():
-            if letter.isupper():
-                if num > ord('Z'):
-                    num -= 26
-                elif num < ord('A'):
-                    num += 26
-            elif letter.islower():
-                if num > ord('z'):
-                    num -= 26
-                elif num < ord('a'):
-                    num += 26
-            result += chr(num)
-        else:
-            result += letter
-    return result
+    def shift_char(self, c, key):
+        if c.isalpha():
+            start = ord('A') if c.isupper() else ord('a')
+            offset = (ord(c) - start + key) % 26
+            return chr(start + offset)
+        return c
 
+    def encrypt(self, message):
+        return ''.join(self.shift_char(c, self.key) for c in message)
 
-def key_req():
-    key = input("Enter a key: ")
-    if key.isdigit():
-        key = int(key)
-        if key in range(1,26):
-            return key
-        else:
-            print("Key must be in range of 1-26")
-            key_req()
-    else:
-        print("Key must be a number")
-        key_req()
+    def decrypt(self, message):
+        return ''.join(self.shift_char(c, -self.key) for c in message)
 
+    def key_req(self):
+        while True:
+            key = input("Enter a key (1-25): ")
+            if key.isdigit():
+                key = int(key)
+                if 1 <= key <= 25:
+                    return key
+                else:
+                    print("Key must be in the range of 1-25.")
+            else:
+                print("Key must be a number.")
 
 def main():
     message = input("Enter a message: ")
-    key = key_req()
-    print("Encrypted message: " + encrypt(message, key))
-    print("Decrypted message: " + decrypt(encrypt(message, key), key))
+    cipher = CaesarCipher()
+    encrypted_message = cipher.encrypt(message)
+    print("Encrypted message: " + encrypted_message)
+    print("Decrypted message: " + cipher.decrypt(encrypted_message))
 
-main()
+if __name__ == "__main__":
+    main()
